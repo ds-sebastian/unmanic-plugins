4e58a3e38fac5774843d5b43b305245c471d158f
Sebastian <sebastian@gbthome.com>
2024-12-09 19:41:02 -0500

    fix type

--- source/seb_custom/plugin.py
+++ source/seb_custom/plugin.py
@@ -36,12 +36,12 @@ class PluginStreamMapper(StreamMapper):
         self.settings = None
 
     def set_default_values(self, settings, abspath, probe):
-        logger.debug(f"Setting default values for file '{abspath}'")
+        logger.info(f"Setting default values for file '{abspath}'")
         self.abspath = abspath
         self.set_probe(probe)
         self.set_input_file(abspath)
         self.settings = settings
-        logger.debug("Default values set.")
+        logger.info("Default values set.")
 
     @staticmethod
     def calculate_bitrate(stream_info: dict):
@@ -50,35 +50,45 @@ class PluginStreamMapper(StreamMapper):
         return calculated_bitrate
 
     def test_stream_needs_processing(self, stream_info: dict):
-        max_rate = int(self.settings.get_setting("max_sample_rate", 48000))
+        # Retrieve the max_sample_rate setting without passing a default to get_setting()
+        value = self.settings.get_setting("max_sample_rate")
+        # If value is None or empty, revert to a default of 48000
+        if value is None:
+            value = 48000
+        max_rate = int(value)
+
         sample_rate = int(stream_info.get("sample_rate", 0))
         codec_name = stream_info.get("codec_name", "unknown")
         channels = stream_info.get("channels", "unknown")
 
-        logger.debug(
+        logger.info(
             f"Testing stream (codec={codec_name}, sample_rate={sample_rate}, "
             f"channels={channels}, max_rate={max_rate}) for processing."
         )
 
         if sample_rate > max_rate:
-            logger.debug(
+            logger.info(
                 f"Stream sample rate {sample_rate} > {max_rate} Hz. Will re-encode."
             )
             return True
 
-        logger.debug(
+        logger.info(
             f"Stream sample rate {sample_rate} <= {max_rate} Hz. No processing needed."
         )
         return False
 
     def custom_stream_mapping(self, stream_info: dict, stream_id: int):
-        max_rate = int(self.settings.get_setting("max_sample_rate", 48000))
+        value = self.settings.get_setting("max_sample_rate")
+        if value is None:
+            value = 48000
+        max_rate = int(value)
+
         calculated_bitrate = self.calculate_bitrate(stream_info)
         channels = int(stream_info.get("channels", 2))
         if channels > 6:
             channels = 6
 
-        logger.debug(
+        logger.info(
             f"Custom mapping for stream {stream_id}: "
             f"re-encode to {self.encoder}, {channels} channels, {calculated_bitrate}k, {max_rate} Hz."
         )
@@ -102,7 +112,7 @@ class PluginStreamMapper(StreamMapper):
 
 def on_library_management_file_test(data):
     abspath = data.get("path")
-    logger.debug(f"on_library_management_file_test called for '{abspath}'")
+    logger.info(f"on_library_management_file_test called for '{abspath}'")
 
     probe = Probe(logger, allowed_mimetypes=["audio", "video"])
     if not probe.file(abspath):
@@ -119,18 +129,18 @@ def on_library_management_file_test(data):
 
     if mapper.streams_need_processing():
         data["add_file_to_pending_tasks"] = True
-        logger.debug(
+        logger.info(
             f"File '{abspath}' requires processing and has been added to the pending tasks."
         )
     else:
-        logger.debug(f"File '{abspath}' does not require processing.")
+        logger.info(f"File '{abspath}' does not require processing.")
 
     return data
 
 
 def on_worker_process(data):
     abspath = data.get("file_in")
-    logger.debug(f"on_worker_process called for '{abspath}'")
+    logger.info(f"on_worker_process called for '{abspath}'")
 
     data["exec_command"] = []
     data["repeat"] = False
@@ -146,20 +156,20 @@ def on_worker_process(data):
     mapper.set_default_values(settings, abspath, probe)
 
     if mapper.streams_need_processing():
-        logger.debug(f"Preparing ffmpeg command for '{abspath}'.")
+        logger.info(f"Preparing ffmpeg command for '{abspath}'.")
 
         mapper.set_input_file(abspath)
         mapper.set_output_file(data.get("file_out"))
 
         ffmpeg_args = mapper.get_ffmpeg_args()
         data["exec_command"] = ["ffmpeg"] + ffmpeg_args
-        logger.debug(f"FFmpeg command: {' '.join(data['exec_command'])}")
+        logger.info(f"FFmpeg command: {' '.join(data['exec_command'])}")
 
         parser = Parser(logger)
         parser.set_probe(probe)
         data["command_progress_parser"] = parser.parse_progress
     else:
-        logger.debug(
+        logger.info(
             f"No processing needed for '{abspath}'. No ffmpeg command will be run."
         )
 
